e0bdfcd7f3a71365e870c04878f4fc3bf25661a3
RamNarayan <108714679+Sameint@users.noreply.github.com>
2023-01-04 23:13:00 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 
 
 module.exports = { API }
+const s = new API('http://api. com/api/hello')
+console. log(s. isSecure()) ;
+
+s.updateUrl('https://api. com/api/hello')
+console. log(s. isSecure());
+
+console.log(s.url);
+
+console.log(s.method);
+
+console.log(s.secure);
